5001d96d0ab59a2a8f42664cba1f094d4570ce17
prashant2071 <pbt9840@gmail.com>
2021-06-12 19:13:20 +0545

    deployment

--- EcommerceApplicationWithNode/db_initialize.js
+++ EcommerceApplicationWithNode/db_initialize.js
@@ -8,7 +8,7 @@ if(process.env.REMOTE){
     conxn_url =  "mongodb+srv://PRASHANT:PRASHANT@123GO@ecommerce.hvvfx.mongodb.net/Ecommerce?retryWrites=true&w=majority";
 }
 console.log('connection url value ',conxn_url)
-mongoose.connect(conxn_url,{
+mongoose.connect(process.env.MONGODB_URI|| conxn_url,{
 useUnifiedTopology:true,useNewUrlParser:true
 },function(err,done){
     if(err){
